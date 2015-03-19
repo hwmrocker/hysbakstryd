1554cd6f8e6978c19b74d41bb776946805a4d97e
Olaf Gladis <olaf@gladis.org>
2015-03-19 22:01:08 +0100

    merge foo

--- client.py
+++ client.py
@@ -70,6 +70,7 @@ class NetworkClient:
     def keyboardinput(self, input_message):
         pass
 
+
 class HWM(NetworkClient):
 
     def __init__(self, *args, **kw):
@@ -84,9 +85,10 @@ class HWM(NetworkClient):
             logging.info("{}: {}, {}".format(from_id, msg_type, ret))
 
         except AttributeError:
+            logging.error("{}({}): {}".format(from_id, msg_type, data))
             if msg_type not in self.ignore_list:
                 self.ignore_list.append(msg_type)
-                logging.warning("No handler for {}".format(msg_type))
+                logging.warning("No handler for '{}'".format(msg_type))
 
     def handle_ERR(self, data):
         logging.error(data)
@@ -98,7 +100,12 @@ class HWM(NetworkClient):
         logging.info(data)
 
     def keyboardinput(self, msg):
-        self.send_msg(dict(type="shout", msg=msg))
+        # foo = ['MovementPhase1', 'ShoutPlugin', 'HelpPlugin']
+        if len(msg) > 1:
+            self.send_msg(dict(type="help_command", command=msg))
+        else:
+            self.send_msg(dict(type="help_command"))
+        # self.send_msg(dict(type="shout", msg=msg))
 
 
 def readshit():
