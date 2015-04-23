ddd32bde839bfbc6cea9a07f10154a945f621c37
Niklas Sombert <niklas@ytvwld.de>
2015-04-23 20:09:37 +0200

    Don't put the hostname into the service name.
    There will be only one server in a network, will it?

--- hysbakstryd/network.py
+++ hysbakstryd/network.py
@@ -4,7 +4,6 @@ import traceback
 import os
 import logging
 import zeroconf
-import platform
 
 # we cannot use from .game import Game because we need to be able to reload it
 import hysbakstryd.game
@@ -141,10 +140,9 @@ class Server:
         This registers a Zeroconf service.
         
         Type: _hysbakstryd._tcp.
-        Name: [hostname]._hysbakstryd._.tcp.
+        Name: Server._hysbakstryd._.tcp.
         """
-        hostname = platform.node()
-        info = zeroconf.ServiceInfo("_hysbakstryd._tcp.", "{}._hysbakstryd._tcp.".format(hostname), port=self.p)
+        info = zeroconf.ServiceInfo("_hysbakstryd._tcp.", "Server._hysbakstryd._tcp.", port=self.p)
         zc = zeroconf()
         zc.register_service(info)
     
