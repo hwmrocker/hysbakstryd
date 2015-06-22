880ce0491ffdae3d004ed473eab407d98cfd0dc2
Johannes Spielmann <j@spielmannsolutions.com>
2015-06-22 15:15:44 +0200

    turn AttrDict into a defaultdict

--- config.py
+++ config.py
@@ -2,15 +2,29 @@
 Configuration for Hysbakstryd server.
 """
 
+
 class AttrDict(dict):
-    """Access a dictionary like a dict OR like a class.
+    """Access a dictionary like a dict OR like a class, and also act like a defaultdict with None as its factory.
 
     Nice little tool, taken from: http://stackoverflow.com/a/14620633
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.__dict__ = self
+
+    def __getattr__(self, item):
+        try:
+            return super().__getattribute__(item)
+        except AttributeError:
+            return None
+
+    def __getitem__(self, item):
+        try:
+            return super().__getitem__(item)
+        except KeyError:
+            return None
 
 d = AttrDict
 
